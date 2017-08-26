09a460dde73c6907fc36383bc06b46493e0e75a6
Michael719 <Michael.trinidad@student.csulb.edu>
2017-08-25 21:29:56 -0700

    Enemies
    - Enemies follow player
    - Enemies can take damage
    - Bullets destroyed after certain time
    - Plane not as bright

--- 3d test/test 3d/Assets/Scripts/BulletController.cs
+++ 3d test/test 3d/Assets/Scripts/BulletController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class BulletController : MonoBehaviour {
 
     public float speed;
+    public float lifetime;
+    public int damageToGive;
 
 	// Use this for initialization
 	void Start () {
@@ -14,5 +16,17 @@ public class BulletController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        lifetime -= Time.deltaTime;
+        if (lifetime <= 0) Destroy(gameObject);
 	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            other.gameObject.GetComponent<EnemyHealthManager>().hurtEnemy(damageToGive);
+            Destroy(gameObject);
+        }
+    }
 }
