e6e4b3ccf907fe0cc28ec47fd734c80959a25cef
Michael719 <Michael.trinidad@student.csulb.edu>
2017-08-25 17:26:45 -0700

    Start
    - Created plane, player, gun, bullet,enemy
    - Gun shoots bullets where mouse is
    - Player holds gun and both face where mouse is

---
+++ 3d test/test 3d/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    private Rigidbody myRigidbody;
+    private Camera mainCamera;
+
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
+    public GunController theGun;
+	// Use this for initialization
+	void Start () {
+        myRigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput * moveSpeed;
+        mainCamera = FindObjectOfType<Camera>();
+
+        Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayLength;
+
+        if (groundPlane.Raycast(cameraRay, out rayLength))
+        {
+            Vector3 pointToLook = cameraRay.GetPoint(rayLength);
+            transform.LookAt(new Vector3(pointToLook.x, transform.position.y, pointToLook.z));
+        }
+
+        if (Input.GetMouseButtonDown(0)) theGun.isFiring = true;
+        if (Input.GetMouseButtonUp(0)) theGun.isFiring = false;
+	}
+
+    private void FixedUpdate()
+    {
+        myRigidbody.velocity = moveVelocity;
+    }
+}
